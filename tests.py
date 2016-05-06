e0d7dfa39f65dbaa3e598bd45b16a068f4792393
Hackbright Student <info@hackbrightacademy.com>
2016-05-06 12:47:12 -0700

    finished not further study

---
+++ tests.py
@@ -0,0 +1,65 @@
+import unittest
+
+from party import app
+from model import db, example_data, connect_to_db
+
+
+class PartyTests(unittest.TestCase):
+    """Tests for my party site."""
+
+    def setUp(self):
+        self.client = app.test_client()
+        app.config['TESTING'] = True
+
+    def test_homepage(self):
+        result = self.client.get("/")
+        self.assertIn("board games, rainbows, and ice cream sundaes", result.data)
+
+    def test_no_rsvp_yet(self):
+        """test RSVP shows up and party details don't"""
+        result = self.client.get("/")
+        self.assertIn("Please RSVP", result.data)
+        self.assertNotIn("Party Details", result.data)
+        
+
+    def test_rsvp(self):
+        result = self.client.post("/rsvp",
+                                  data={'name': "Jane", 'email': "jane@jane.com"},
+                                  follow_redirects=True)
+        self.assertNotIn("Please RSVP", result.data)
+        self.assertIn("Party Details", result.data)
+
+
+class PartyTestsDatabase(unittest.TestCase):
+    """Flask tests that use the database."""
+
+    def setUp(self):
+        """Stuff to do before every test."""
+
+        self.client = app.test_client()
+        app.config['TESTING'] = True
+
+        # Connect to test database (uncomment when testing database)
+        connect_to_db(app, "postgresql:///testdb")
+
+        # Create tables and add sample data (uncomment when testing database)
+        db.create_all()
+        example_data()
+
+    def tearDown(self):
+        """Do at end of every test."""
+
+        # (uncomment when testing database)
+        db.session.close()
+        db.drop_all()
+
+    def test_games(self):
+        """Check that test game in test database"""
+
+        result = self.client.get("/games")
+        self.assertIn("Test Game 1", result.data)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
